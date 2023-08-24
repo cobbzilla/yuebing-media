b6a19b9199fc41db0fe3847dafe57a6433a6509a
Jonathan Cobb <jonathan@kyuss.org>
2023-08-23 23:29:54 -0400

    remove media operations and properties

--- lib/esm/type.d.ts
+++ lib/esm/type.d.ts
@@ -1,26 +1,15 @@
 import { MediaProfileType } from "yuebing-model";
-import { MobilettoOrmTypeDef } from "mobiletto-orm";
-import { ParsedProperties } from "./driver";
 import { MobilettoConnection } from "mobiletto-base";
 export type ApplyProfileResponse = {
     args?: string[];
     analysis?: object | string | number | boolean;
 };
-export type MediaProperties = Record<string, object | string | number | boolean>;
-export type MediaOperation = {
-    name: string;
-    analysis?: boolean;
-    command?: string;
-    func?: boolean;
-    minFileSize: number;
-};
-export type MediaOperationConfig = Record<string, string | number | boolean>;
 export type ParsedProfile = MediaProfileType & {
     subProfileObjects?: ParsedProfile[];
     additionalAssetsRegexes?: RegExp[];
+    operationConfigObject?: Record<string, string | number | boolean | object>;
 };
 export type MediaOperationFunc = (downloaded: string, profile: ParsedProfile, outDir: string, sourcePath: string, conn: MobilettoConnection) => Promise<ApplyProfileResponse>;
 export type MediaPlugin = {
     applyProfile: MediaOperationFunc;
-    operationConfigType: (operation: MediaOperation, parsedProps: ParsedProperties) => MobilettoOrmTypeDef;
 };
--- src/type.ts
+++ src/type.ts
@@ -1,6 +1,4 @@
 import { MediaProfileType } from "yuebing-model";
-import { MobilettoOrmTypeDef } from "mobiletto-orm";
-import { ParsedProperties } from "./driver";
 import { MobilettoConnection } from "mobiletto-base";
 
 export type ApplyProfileResponse = {
@@ -8,21 +6,10 @@ export type ApplyProfileResponse = {
     analysis?: object | string | number | boolean;
 };
 
-export type MediaProperties = Record<string, object | string | number | boolean>;
-
-export type MediaOperation = {
-    name: string;
-    analysis?: boolean;
-    command?: string;
-    func?: boolean;
-    minFileSize: number;
-};
-
-export type MediaOperationConfig = Record<string, string | number | boolean>;
-
 export type ParsedProfile = MediaProfileType & {
     subProfileObjects?: ParsedProfile[];
     additionalAssetsRegexes?: RegExp[];
+    operationConfigObject?: Record<string, string | number | boolean | object>;
 };
 
 export type MediaOperationFunc = (
@@ -35,5 +22,4 @@ export type MediaOperationFunc = (
 
 export type MediaPlugin = {
     applyProfile: MediaOperationFunc;
-    operationConfigType: (operation: MediaOperation, parsedProps: ParsedProperties) => MobilettoOrmTypeDef;
 };
